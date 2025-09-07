fbc1855e8040bbb12eb4eec9df1f3f457c3abdd8
mjh-shikder <mjhsikder@gmail.com>
2025-09-07 12:16:25 +0600

    dynamic word cards added

--- script/index.js
+++ script/index.js
@@ -4,6 +4,46 @@ const loadLessons = () => {
         .then(json => displayLesson(json.data));
 }
 
+const loadLevelWord = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayLevelWord(data.data))
+    
+}
+
+/**
+ * "id": 15,
+"level": 5,
+"word": "Obstinate",
+"meaning": "একগুঁয়ে",
+"pronunciation": "অবস্টিনেট"
+ */
+
+
+const displayLevelWord = (words) => {
+    const wordContaienr = document.getElementById('word-container');
+    wordContaienr.innerHTML = "";
+
+    words.forEach(word => {
+        const card = document.createElement('div');
+        card.innerHTML = `
+         <div class="bg-white rounded-xl shadow-md text-center py-10 px-6 space-y-4">
+          <h2 class="font-bold text-2xl">${word.word}</h2>
+          <p class="font-semibold ">Meaning or proronoi</p>
+
+         <div class="font-bangla text-2xl font-medium text-gray-600">"${word.meaning} / ${word.pronunciation}"</div>
+         <div class="flex justify-between items-center">
+          <button class="btn bg-[#e9f4ff10] hover:bg-[#e9f4ff]"><i class="fa-solid fa-circle-info text-gray-600"></i></button>
+          <button class="btn bg-[#e9f4ff10] hover:bg-[#e9f4ff]"><i class="fa-solid fa-volume-low text-gray-600"></i></button>
+         </div>
+        </div>
+        `
+        wordContaienr.appendChild(card)
+        
+    })
+    
+}
 
 const displayLesson = (lessons) => {
     // 1 ge the container & empty
@@ -15,7 +55,7 @@ const displayLesson = (lessons) => {
       // 3 Create element 
         const btnDiv = document.createElement('div');
         btnDiv.innerHTML = `
-        <button  class="btn btn-outline btn-primary">
+        <button onclick= "loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
         <i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</button>
         `;
     
